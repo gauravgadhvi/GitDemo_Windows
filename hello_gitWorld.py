8c6ae2bf85e948eebea0386db4e5cc7d16bd1884
Gaurav Gadhvi <ggadhvi@hawk.iit.edu>
2022-03-21 17:25:41 -0500

    New python script for hello world

---
+++ hello_gitWorld.py
@@ -0,0 +1,13 @@
+### This is a demo file for initializing the tutorial repo ###
+
+
+import os
+import sys
+import datetime
+
+
+
+print("Today's date is :", datetime.date.today())
+
+print("What tims is it right now?!")
+
